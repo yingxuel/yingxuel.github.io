24d08824aac77940cb5acb39df04aa3a19471914
Yingxue Li <yingxue@yingxue-macbookair.roam.corp.google.com>
2017-03-03 17:20:03 -0500

    sjflkd

--- MerylReceiver/player.js
+++ MerylReceiver/player.js
@@ -1,114 +1,401 @@
 'use strict';
 
-var sampleplayer = sampleplayer || {};
 /**
- * @param {!Element} element the element to attach the player
- * @struct
- * @constructor
- * @export
+ * Entry point for the sample video player which uses media element for
+ * rendering video streams.
+ *
+ * @this {Player}
+ * @param {!HTMLMediaElement} mediaElement for video rendering.
  */
-sampleplayer.CastPlayer = function(element) {
+var Player = function(mediaElement) {
   var namespace = 'urn:x-cast:com.google.ads.interactivemedia.dai.cast';
-  this.mediaElement_ = element;
-
-  this.player_ = null;
-
+  var self = this;
+  this.useSdk_ = false;
+  this.adNum_ = 1;
+  this.breakNum_ = 1;
+  this.castPlayer_ = null;
+  this.seekToTimeAfterAdBreak_ = 0;
+  this.startTime_ = 0;
+  this.adIsPlaying_ = false;
+  this.mediaElement_ = mediaElement;
   this.receiverManager_ = cast.receiver.CastReceiverManager.getInstance();
+  this.receiverManager_.onSenderConnected = function(event) {
+    console.log('Sender Connected');
+  };
   this.receiverManager_.onSenderDisconnected =
-    this.onSenderDisconnected_.bind(this);
+      this.onSenderDisconnected.bind(this);
+  /*this.imaMessageBus_ = this.receiverManager_.getCastMessageBus(namespace);
+  this.imaMessageBus_.onMessage = function(event) {
+    console.log('Received message from sender: ' + event.data);
+    var message = event.data.split(',');
+    var method = message[0];
+    switch (method) {
+      case 'bookmark':
+        var time = parseFloat(message[1]);
+        self.bookmark_(time);
+        break;
+      case 'seek':
+        var time = parseFloat(message[1]);
+        self.seek_(time);
+        break;
+      case 'snapback':
+        var time = parseFloat(message[1]);
+        self.snapback_(time);
+        break;
+      case 'getContentTime':
+        var contentTime = self.getContentTime_();
+        self.broadcast_('contentTime,' + contentTime);
+        break;
+      case 'requestStream':
+        self.requestStream_();
+        break;
+      default:
+        self.broadcast_('Message not recognized');
+        break;
+    }
+  };*/
 
   this.mediaManager_ = new cast.receiver.MediaManager(this.mediaElement_);
-  this.mediaManager_.onLoad = this.onLoad_.bind(this);
+  this.mediaManager_.onLoad = this.onLoad.bind(this);
+  this.mediaManager_.onSeek = this.onSeek.bind(this);
+  if (this.useSdk_) {
+    this.initStreamManager_();
+  }
 };
 
-sampleplayer.CastPlayer.prototype.start = function() {
+/**
+ * Initializes receiver stream manager and adds callbacks.
+ * @private
+ */
+Player.prototype.initStreamManager_ = function() {
+  var self = this;
+  this.streamManager_ =
+      new google.ima.dai.api.StreamManager(this.mediaElement_);
+  var onStreamDataReceived = this.onStreamDataReceived.bind(this);
+  var sendPingForTesting = this.sendPingForTesting_.bind(this);
+  this.streamManager_.addEventListener(
+      google.ima.dai.api.StreamEvent.Type.LOADED,
+      function(event) {
+        var streamUrl = event.getStreamData().url;
+        console.log("Stream URL: " + streamUrl);
+        // Each element in subtitles array is an object with url and language
+        // properties. Example of a subtitles array with 2 elements:
+        // {
+        //   "url": "http://www.sis.com/1234/subtitles_en.ttml",
+        //   "language": "en"
+        // }, {
+        //   "url": "http://www.sis.com/1234/subtitles_fr.ttml",
+        //   "language": "fr"
+        // }
+        self.subtitles = event.getStreamData().subtitles;
+        onStreamDataReceived(streamUrl);
+      },
+      false);
+  this.streamManager_.addEventListener(
+      google.ima.dai.api.StreamEvent.Type.STREAM_INITIALIZED,
+      function(event) {
+        self.sendPingForTesting_('streamInit', self.adNum_);
+      },
+      false);
+  this.streamManager_.addEventListener(
+      google.ima.dai.api.StreamEvent.Type.ERROR,
+      function(event) {
+        var errorMessage = event.getStreamData().errorMessage;
+        self.broadcast_(errorMessage);
+        var errorCode = /4\d{2}/.exec(errorMessage)[0];
+        self.sendPingForTesting_('error?code=' + errorCode);
+        console.log(event);
+      },
+      false);
+  this.streamManager_.addEventListener(
+      google.ima.dai.api.StreamEvent.Type.CUEPOINTS_CHANGED,
+      function(event) {
+        console.log("Cuepoints changed: ");
+        console.log(event.getStreamData());
+      },
+      false);
+  this.streamManager_.addEventListener(
+      google.ima.dai.api.StreamEvent.Type.STARTED,
+      function(event) {
+        self.broadcast_('started');
+        sendPingForTesting('start', self.adNum_);
+        var ad = event.getAd();
+        var adPodInfo = ad ? ad.getAdPodInfo() : null;
+        var title = ad ? ad.getTitle() : '<no-title>';
+        var position = adPodInfo ? adPodInfo.getAdPosition() : 0;
+        console.log('Ad Title: ' + title);
+        console.log('Ad Position: ' + position);
+      },
+      false);
+  this.streamManager_.addEventListener(
+      google.ima.dai.api.StreamEvent.Type.FIRST_QUARTILE,
+      function(event) {
+        sendPingForTesting('first', self.adNum_);
+      },
+      false);
+  this.streamManager_.addEventListener(
+      google.ima.dai.api.StreamEvent.Type.MIDPOINT,
+      function(event) {
+        sendPingForTesting('mid', self.adNum_);
+      },
+      false);
+  this.streamManager_.addEventListener(
+      google.ima.dai.api.StreamEvent.Type.THIRD_QUARTILE,
+      function(event) {
+        sendPingForTesting('third', self.adNum_);
+      },
+      false);
+  this.streamManager_.addEventListener(
+      google.ima.dai.api.StreamEvent.Type.COMPLETE,
+      function(event) {
+        self.broadcast_('complete');
+        sendPingForTesting('complete', self.adNum_);
+        self.adNum_++;
+      },
+      false);
+  this.streamManager_.addEventListener(
+      google.ima.dai.api.StreamEvent.Type.AD_BREAK_STARTED,
+      function(event) {
+        self.adIsPlaying_ = true;
+        document.getElementById('ad-ui').style.display = 'block';
+        sendPingForTesting('adBreakStarted\?num=' + self.breakNum_);
+        self.broadcast_('ad_break_started');
+      },
+      false);
+  this.streamManager_.addEventListener(
+      google.ima.dai.api.StreamEvent.Type.AD_BREAK_ENDED,
+      function(event) {
+        self.adIsPlaying_ = false;
+        document.getElementById('ad-ui').style.display = 'none';
+        sendPingForTesting('adBreakEnded\?num=' + self.breakNum_);
+        self.breakNum_++;
+        self.broadcast_('ad_break_ended');
+        if (self.seekToTimeAfterAdBreak_ > 0) {
+          self.seek_(self.seekToTimeAfterAdBreak_);
+          self.seekToTimeAfterAdBreak_ = 0;
+        }
+      },
+      false);
+  this.streamManager_.addEventListener(
+      google.ima.dai.api.StreamEvent.Type.AD_PROGRESS,
+      function(event) {
+        var adData = event.getStreamData().adProgressData;
+        document.getElementById('ad-position').innerHTML
+          = adData.adPosition;
+        document.getElementById('total-ads').innerHTML
+          = adData.totalAds;
+        document.getElementById('time-value').innerHTML
+          = Math.ceil(parseFloat(adData.duration)
+            - parseFloat(adData.currentTime));
+        document.getElementById('ad-ui').style.display = 'block';
+      },
+      false);
+};
+
+
+/**
+ * Gets content time for the stream.
+ * @returns {number} The content time.
+ * @private
+ */
+Player.prototype.getContentTime_ = function() {
+  return this.streamManager_
+      .contentTimeForStreamTime(this.mediaElement_.currentTime);
+};
+
+
+/**
+ * Sends event ping for testing.
+ * @param {!string} event Event pinged.
+ * @param {number} number The ad number.
+ * @private
+ */
+Player.prototype.sendPingForTesting_ = function(event, number) {
+  var testingPing = 'http://www.example.com/' + event;
+  if (number) {
+    testingPing += '@?num=' + number + 'ld';
+  }
+  var xmlhttp = new XMLHttpRequest();
+  //xmlhttp.open('GET', testingPing, true);
+  //xmlhttp.send();
+  //this.broadcast_('Pinging url: ' + testingPing);
+};
+
+
+/**
+ * Sends messages to all connected sender apps.
+ * @param {!string} message Message to be sent to senders.
+ * @private
+ */
+Player.prototype.broadcast_ = function(message) {
+  if (this.imaMessageBus_ && this.imaMessageBus_.broadcast) {
+    this.imaMessageBus_.broadcast(message);
+  }
+};
+
+
+/**
+ * Starts receiver manager which tracks playback of the stream.
+ */
+Player.prototype.start = function() {
   this.receiverManager_.start();
 };
 
 /**
- * Loads some video content.
- *
- * @param {!cast.receiver.MediaManager.LoadInfo} info The load request info.
- * @return {boolean} Whether the media was preloaded
- * @private
+ * Called when a sender disconnects from the app.
+ * @param {cast.receiver.CastReceiverManager.SenderDisconnectedEvent} event
  */
-sampleplayer.CastPlayer.prototype.loadVideo_ = function() {
-  console.log('loadVideo_');
-  // MD Cast SDK Integration
-  // Set withCredentials = true if the request to your server or CDN requires passong cookies 
-  // when requesting playlists, encryption keys or segments.
-  var updateManifestRequestInfoCallback = function(requestInfo) {
+Player.prototype.onSenderDisconnected = function(event) {
+  console.log('onSenderDisconnected');
+  // When the last or only sender is connected to a receiver,
+  // tapping Disconnect stops the app running on the receiver.
+  if (this.receiverManager_.getSenders().length === 0 &&
+      event.reason ===
+          cast.receiver.system.DisconnectReason.REQUESTED_BY_SENDER) {
+    this.receiverManager_.stop();
+  }
+};
+
+
+/**
+ * Called when we receive a LOAD message from the sender.
+ * @param {!cast.receiver.MediaManager.Event} event The load event.
+ */
+Player.prototype.onLoad = function(event) {
+  var imaRequestData = event.data.media.customData;
+  this.startTime_ = imaRequestData.startTime;
+  if (imaRequestData.assetKey) {
+    this.streamRequest =
+      new google.ima.dai.api.LiveStreamRequest(imaRequestData);
+  } else if (imaRequestData.contentSourceId) {
+    this.streamRequest =
+      new google.ima.dai.api.VODStreamRequest(imaRequestData);
+      console.log(this.streamRequest);
+  }
+  document.getElementById('splash').style.display = 'none';
+  this.requestStream_();
+};
+
+Player.prototype.requestStream_ = function() {
+  if (this.useSdk_) {
+    this.streamManager_.requestStream(this.streamRequest);
+  } else {
+    this.onStreamDataReceived('');
+  }
+};
+
+
+/**
+ * Processes the SEEK event from the sender.
+ * @param {!cast.receiver.MediaManager.Event} event The seek event.
+ * @this {Player}
+ */
+Player.prototype.onSeek = function(event) {
+  var currentTime = event.data.currentTime;
+  this.snapback_(currentTime);
+  this.mediaManager_.broadcastStatus(true, event.data.requestId);
+};
+
+
+/**
+ * Loads stitched ads+content stream.
+ * @param {!string} url of the stream.
+ */
+Player.prototype.onStreamDataReceived = function(url) {
+  var self = this;
+  var host = new cast.player.api.Host({
+    //'url': url,
+    'url': 'https://cbsdaistg-vh.akamaihd.net/i/temp_hd_gallery_video/CBS_Production_Outlet_VMS/video_robot/CBS_Production_Entertainment/2017/02/19/880378435780/CBS_2_BROKE_GIRLS_617_CONTENT_CIAN_vr_20M_1053680_,1848000,548000,158000,2596000,1248000,298000,3596000,848000,.mp4.csmil/master.m3u8?hdnea=st=1488576644~exp=1488580244~acl=/i/temp_hd_gallery_video/CBS_Production_Outlet_VMS/video_robot/CBS_Production_Entertainment/2017/02/19/880378435780/CBS_2_BROKE_GIRLS_617_CONTENT_CIAN_vr_20M_1053680_,1848000,548000,158000,2596000,1248000,298000,3596000,848000,.mp4.csmil/*~hmac=4c0a20013460e0bf4b39a32dbc761aa76818ef9b2b1c476499b2bdd4a1b7fa3d&originpath=/ondemand/hls/content/6067/vid/C1BDCF7F-2B9C-4F05-1009-53D6F0549AA3/CHS/streams/0375b5df-9ad6-46d3-8f85-1fda4c58dc5b/master.m3u8',
+    'mediaElement': this.mediaElement_
+  });
+  //this.broadcast_('onStreamDataReceived: ' + url);
+  host.processMetadata = function(type, data, timestamp) {
+    if (self.useSdk_) {
+      self.streamManager_.processMetadata(type, data, timestamp);
+    }
+  };
+  host.updateManifestRequestInfo = function(requestInfo) {
     if (!requestInfo.url) {
       requestInfo.url = this.url;
     }
-    console.log('### updateManifestRequestInfo: load manifest url - ' + requestInfo.url);
     if (requestInfo.url.indexOf("googlevideo.com") != -1
       || requestInfo.url.indexOf("cbsdai-ads.akamaized.net") != -1) {
       requestInfo.withCredentials = false;
     } else {
       requestInfo.withCredentials = true;
     }
+    console.log('update manifest: ' + requestInfo.withCredentials);
   };
-  var updateLicenseRequestInfoCallback = function(requestInfo) {
-    console.log('### updateLicenseRequestInfo: request url - ' + requestInfo.url);
+  host.updateLicenseRequestInfo = function(requestInfo) {
     if (requestInfo.url.indexOf("googlevideo.com") != -1
       || requestInfo.url.indexOf("cbsdai-ads.akamaized.net") != -1) {
       requestInfo.withCredentials = false;
     } else {
       requestInfo.withCredentials = true;
     }
+    console.log('update license: ' + requestInfo.withCredentials);
   };
-  var updateSegmentRequestInfoCallback = function(requestInfo) {
-    console.log('### updateSegmentRequestInfo: request url - ' + requestInfo.url);
-    if (requestInfo.url.indexOf("googlevideo.com") != -1 || requestInfo.url.indexOf("cbsdai-ads.akamaized.net") != -1) {
+  host.updateSegmentRequestInfoCallback = function(requestInfo) {
+    if (requestInfo.url.indexOf("googlevideo.com") != -1
+      || requestInfo.url.indexOf("cbsdai-ads.akamaized.net") != -1) {
       requestInfo.withCredentials = false;
     } else {
       requestInfo.withCredentials = true;
     }
+    console.log('update segment: ' + requestInfo.withCredentials);
   };
-  // MD Cast SDK Integration
-  var host = new cast.player.api.Host({
-    'mediaElement': this.mediaElement_,
-    'url': 'https://cbsdaistg-vh.akamaihd.net/i/temp_hd_gallery_video/CBS_Production_Outlet_VMS/video_robot/CBS_Production_Entertainment/2017/02/19/880378435780/CBS_2_BROKE_GIRLS_617_CONTENT_CIAN_vr_20M_1053680_,1848000,548000,158000,2596000,1248000,298000,3596000,848000,.mp4.csmil/master.m3u8?hdnea=st=1488578720~exp=1488582320~acl=/i/temp_hd_gallery_video/CBS_Production_Outlet_VMS/video_robot/CBS_Production_Entertainment/2017/02/19/880378435780/CBS_2_BROKE_GIRLS_617_CONTENT_CIAN_vr_20M_1053680_,1848000,548000,158000,2596000,1248000,298000,3596000,848000,.mp4.csmil/*~hmac=b294ee3cc6533132ef0d441ce0d346faf6c6c5625beb450d697f0bad08423458&originpath=/ondemand/hls/content/6067/vid/C1BDCF7F-2B9C-4F05-1009-53D6F0549AA3/CHS/streams/818f31ad-5dea-48f6-bd54-302991be44d2/master.m3u8',
-  });
-  host.updateManifestRequestInfo = updateManifestRequestInfoCallback;
-  host.updateLicenseRequestInfo = updateLicenseRequestInfoCallback;
-  host.updateSegmentRequestInfo = updateSegmentRequestInfoCallback;
-
-  this.player_ = new cast.player.api.Player(host);
-  this.player_.load(cast.player.api.CreateHlsStreamingProtocol(host), 0);
+  var currentTime = this.startTime_ > 0 ? this.streamManager_
+    .streamTimeForContentTime(this.startTime_) : 0;
+  //this.broadcast_('start time: ' + currentTime);
+  this.castPlayer_ = new cast.player.api.Player(host);
+  this.castPlayer_.load(
+    cast.player.api.CreateHlsStreamingProtocol(host));//, currentTime);
+  /*if (this.subtitles[0] && this.subtitles[0].ttml) {
+    this.castPlayer_.enableCaptions(true, 'ttml', this.subtitles[0].ttml);
+  }*/
 };
 
 /**
- * Called when a sender disconnects from the app.
- *
- * @param {cast.receiver.CastReceiverManager.SenderDisconnectedEvent} event
+ * Bookmarks content so stream will return to this location if revisited.
  * @private
  */
-sampleplayer.CastPlayer.prototype.onSenderDisconnected_ = function(event) {
-  console.log('onSenderDisconnected');
-  this.receiverManager_.stop();
-};
-/**
- * Called when media has an error. Transitions to IDLE state and
- * calls to the original media manager implementation.
- *
- * @see cast.receiver.MediaManager#onError
- * @param {!Object} error
- * @private
- */
-sampleplayer.CastPlayer.prototype.onError_ = function(error) {
-  console.log('onError: ' + error);
+Player.prototype.bookmark_ = function() {
+  this.broadcast_('Current Time: ' + this.mediaElement_.currentTime);
+  var bookmarkTime = this.streamManager_
+    .contentTimeForStreamTime(this.mediaElement_.currentTime);
+  this.broadcast_('bookmark,' + bookmarkTime);
 };
 
 /**
- * Called when we receive a LOAD message. Calls load().
- *
- * @see sampleplayer#load
- * @param {cast.receiver.MediaManager.Event} event The load event.
+ * Seeks player to location.
+ * @param {number} time The time to seek to in seconds.
  * @private
  */
-sampleplayer.CastPlayer.prototype.onLoad_ = function(event) {
-  console.log('onLoad_');
-  this.loadVideo_();
+Player.prototype.seek_ = function(time) {
+  if (this.adIsPlaying_) {
+    return;
+  }
+  this.mediaElement_.currentTime = time;
+  this.broadcast_('Seeking to: ' + time);
+};
+
+/**
+ * Seeks player to location and plays last ad break if it has not been
+ * seen already.
+ * @param {number} time The time to seek to in seconds.
+ * @private
+ */
+Player.prototype.snapback_ = function(time) {
+  var previousCuepoint =
+    this.streamManager_.previousCuePointForStreamTime(time);
+  console.log(previousCuepoint);
+  var played = previousCuepoint.played;
+  if (played) {
+    this.seek_(time);
+  } else {
+    // Adding 0.1 to cuepoint start time because of bug where stream freezes
+    // when seeking to certain times in VOD streams.
+    this.seek_(previousCuepoint.start + 0.1);
+    this.seekToTimeAfterAdBreak_ = time;
+  }
 };
